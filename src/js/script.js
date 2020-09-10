acd834a48595703d42e2442bb5efdcaf742e452b
Jolanta Piaseczna <piaseczna@onet.pl>
2020-09-10 15:30:01 +0200

    Add new Product

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,11 @@
   }
 
   const app = {
+    initMenu: function() {
+      const testProduct = new Product();
+      console.log('testProduct:', testProduct);
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -68,6 +73,8 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initMenu();
     },
   };
 
