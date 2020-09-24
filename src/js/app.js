77025349c9a7e18a370cceb2141437e26f43dfab
Jolanta Piaseczna <piaseczna@onet.pl>
2020-09-24 21:01:31 +0200

    Add subpage address settings

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import {Product} from './components/Product.js';
 import {Cart} from './components/Cart.js';
-import {select, settings} from './settings.js';
+import {select, settings, classNames} from './settings.js';
 
 const app = {
   initMenu: function () {
@@ -47,6 +47,7 @@ const app = {
         console.log('settings:', settings);
         console.log('templates:', templates); */
 
+    thisApp.initPages();    
     thisApp.initData();
     thisApp.initCart();
   },
@@ -62,6 +63,53 @@ const app = {
     thisApp.productList.addEventListener('add-to-cart', function(event) {
       app.cart.add(event.detail.product);
     });
+  },
+
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+
+    let pagesMatchingHash = [];
+
+    if(window.location.hash.length > 2) {
+      const idFromHash = window.location.hash.replace('#', '');
+
+      pagesMatchingHash = thisApp.pages.filter(function(page) {
+        return page.id == idFromHash;
+      }); 
+    }
+
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* TODO: get page id from href */
+        const pageId = clickedElement.getAttribute('href').replace('#', '');
+        //const pageId = pageHREF.replace('#', ' ');
+
+        /* TODO: activate page */
+        thisApp.activatePage(pageId);
+      });
+    }
+
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+
+    for(let page of thisApp.pages) {
+      page.classList.toggle(classNames.nav.active, page.getAttribute('href') == pageId);
+    }
+
+    window.location.hash = '#/' + pageId;
   },
 };
 
