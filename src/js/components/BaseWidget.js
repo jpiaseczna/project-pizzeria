e9160ddd99fabcdda22eca22aa4cc5349b62feef
Jolanta Piaseczna <piaseczna@onet.pl>
2020-09-28 18:54:37 +0200

    Add class DatePicker

--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -2,51 +2,51 @@ export class BaseWidget {
   constructor(wrapperElement, initialValue) {
     const thisWidget = this;
 
-    thisWidget.dom = {}
-	thisWidget.dom.wrapper = wrapperElement;
-	thisWidget.correctValue = initialValue;    	
+    thisWidget.dom = {};
+    thisWidget.dom.wrapper = wrapperElement;
+    thisWidget.correctValue = initialValue;
   }
 
   get value() {
-  	const thisWidget = this;
+    const thisWidget = this;
 
-  	return thisWidget.correctValue;
+    return thisWidget.correctValue;
   }
 
   set value(assignedValue) {
-  	const thisWidget = this;
+    const thisWidget = this;
 
-  	const newValue = thisWidget.parseValue(assignedValue);
+    const newValue = thisWidget.parseValue(assignedValue);
 
-  	if(newValue != thisWidget.correctValue && thisWidget.isValid(newValue)) {
-  		thisWidget.correctValue = newValue;
-  		thisWidget.announce();
-  	}
+    if (newValue != thisWidget.correctValue && thisWidget.isValid(newValue)) {
+      thisWidget.correctValue = newValue;
+      thisWidget.announce();
+    }
 
-  	thisWidget.renderValue();
+    thisWidget.renderValue();
   }
 
   parseValue(newValue) {
-  	return parseInt(newValue);
+    return parseInt(newValue);
   }
 
   isValid(newValue) {
-  	return !isNaN(newValue);
+    return !isNaN(newValue);
   }
 
   renderValue() {
-  	const thisWidget = this;
+    const thisWidget = this;
 
-  	console.log('widget.value:', thisWidget.value);
+    console.log('widget.value:', thisWidget.value);
   }
 
   announce() {
-  	const thisWidget = this;
+    const thisWidget = this;
 
-  	const event = new CustomEvent('updated', {
-  		bubbles: true
-  	});
+    const event = new CustomEvent('updated', {
+      bubbles: true,
+    });
 
-  	thisWidget.dom.wrapper.dispatchEvent(event);
+    thisWidget.dom.wrapper.dispatchEvent(event);
   }
-}  
+} 
--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,6 @@
 import { templates, select } from '../settings.js';
 import { AmountWidget } from './AmountWidget.js';
+import { DatePicker } from './DatePicker.js';
 
 export class Booking {
   constructor(bookingWidgetContainer) {
@@ -19,6 +20,7 @@ export class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
   }
 
   initWidgets() {
@@ -26,5 +28,6 @@ export class Booking {
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
   }
 }
